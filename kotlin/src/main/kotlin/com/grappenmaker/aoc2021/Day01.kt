88e41dca2d8e4848857694de3d88cf1cc84d498f
770grappenmaker <770grappenmaker@gmail.com>
2021-12-01 17:41:48 +0100

    Kotlin - Day 1 - Ah it's beautiful

--- kotlin/src/main/kotlin/com/grappenmaker/aoc2021/Day01.kt
+++ kotlin/src/main/kotlin/com/grappenmaker/aoc2021/Day01.kt
@@ -3,15 +3,12 @@ package com.grappenmaker.aoc2021
 fun solveDay1() {
     // Part one
     val input = getInputLines(1).map { it.toInt() }
-    val countIncrements = { arr: List<Int> ->
-        arr.filterIndexed { i, n -> if (i == 0) false else n > arr[i - 1] }.size
-    }
+    val countIncrements = { arr: List<Int> -> arr.asSequence().drop(1).filterIndexed { i, n -> n > arr[i] }.count() }
 
     val increments = countIncrements(input)
     println("Part one: $increments")
 
     // Part two
-    val sumIncrements =
-        countIncrements(input.mapIndexed { i, _ -> if (i + 2 >= input.size) 0 else input.slice(i..i + 2).sum() })
+    val sumIncrements = countIncrements(input.windowed(3).map { it.sum() })
     println("Part two: $sumIncrements")
 }
