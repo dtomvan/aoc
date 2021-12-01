70c3909b31a8f6b2d39865f255080f3b9d760e8a
Tom van Dijk <18gatenmaker6@gmail.com>
2021-12-01 16:50:21 +0100

    Rust - Day 01

--- rust/src/days/day01.rs
+++ rust/src/days/day01.rs
@@ -1,4 +1,22 @@
 pub fn main() -> anyhow::Result<()> {
-    let _input = include_str!("../../../inputs/day-1.txt");
+    // Part 1
+    let input = include_str!("../../../inputs/day-1.txt");
+    let numbers = input.lines().filter_map(|x| x.parse::<usize>().ok());
+    println!("Part 1: {}", solution(&numbers));
+
+    // Part 2
+    let solution = solution(
+        &numbers
+            .collect::<Vec<_>>()
+            .windows(3)
+            .map(|x| x.iter().sum()),
+    );
+    println!("Part 2: {}", solution);
     Ok(())
+}
+
+fn solution<I: Iterator<Item = usize> + Clone>(iter: &I) -> usize {
+    iter.clone().zip(iter.clone().skip(1))
+        .filter(|x| x.0 < x.1)
+        .count()
 }
