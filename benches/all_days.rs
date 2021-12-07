ff47011b99e78c6b6cb5cf25d06ca94fa96c3b2e
Tom van Dijk <18gatenmaker6@gmail.com>
2021-12-07 15:08:09 +0100

    Day 7

--- benches/all_days.rs
+++ benches/all_days.rs
@@ -1,14 +1,33 @@
 use aoc_2021::days::*;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+pub fn day_1(c: &mut Criterion) {
     c.bench_function("day 1", |b| b.iter(|| day01::main()));
+}
+
+pub fn day_2(c: &mut Criterion) {
     c.bench_function("day 2", |b| b.iter(|| day02::main()));
+}
+
+pub fn day_3(c: &mut Criterion) {
     c.bench_function("day 3", |b| b.iter(|| day03::main()));
+}
+
+pub fn day_4(c: &mut Criterion) {
     c.bench_function("day 4", |b| b.iter(|| day04::main()));
+}
+
+pub fn day_5(c: &mut Criterion) {
     c.bench_function("day 5", |b| b.iter(|| day05::main()));
+}
+
+pub fn day_6(c: &mut Criterion) {
     c.bench_function("day 6", |b| b.iter(|| day06::main()));
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+pub fn day_7(c: &mut Criterion) {
+    c.bench_function("day 7", |b| b.iter(|| day07::main()));
+}
+
+criterion_group!(all_days, day_1, day_2, day_3, day_4, day_5, day_6, day_7);
+criterion_main!(all_days);
--- src/days/day07.rs
+++ src/days/day07.rs
@@ -1,5 +1,31 @@
-pub fn main() -> anyhow::Result<(usize, usize)> {
-    let _input = include_str!("../../inputs/day-7.txt");
+use itertools::Itertools;
 
-    Err(anyhow::anyhow!("Not implemented."))
+pub fn main() -> anyhow::Result<(usize, usize)> {
+    let mut input = include_str!("../../inputs/day-7.txt")
+        .trim()
+        .split(',')
+        .filter_map(|x| x.parse::<isize>().ok())
+        .collect_vec();
+
+    input.sort_unstable();
+
+    let half_len = input.len() / 2;
+    let mean = ((input[half_len] + input[(half_len) + input.len() % 2]) / 2) as isize;
+    let total_fuel = input.iter().fold(0, |acc, x| acc + (x - mean).abs());
+
+    let part_2 = (input[0]..input[input.len() - 1]).fold(0, |acc, x| {
+        let fuel = input.iter().fold(0, |acc, y| acc + sum((x - y).abs()));
+
+        if fuel < acc || acc == 0 {
+            fuel
+        } else {
+            acc
+        }
+    });
+
+    Ok((total_fuel as usize, part_2 as usize))
+}
+
+fn sum(n: isize) -> isize {
+    (n * (n + 1)) / 2
 }
