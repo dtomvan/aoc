2103ef1d230683afa4c35de7ee31bfd5144e28a6
Tom van Dijk <18gatenmaker6@gmail.com>
2022-12-02 07:14:17 +0100

    remove imaginary day 2022-00

--- year/2022/src/_match_days.rs
+++ year/2022/src/_match_days.rs
@@ -1,5 +1,4 @@
 match arg.as_str() {
-	"0" => day0::main,
 	"1" => day1::main,
 	"2" => day2::main,
     _ => unimplemented!(),
--- year/2022/src/days/day0.rs
+++
@@ -1,5 +0,0 @@
-use aoc_common::result::{done, AocResult};
-
-pub fn main() -> AocResult {
-    done("STUB".to_string(), "DAY".to_string())
-}
